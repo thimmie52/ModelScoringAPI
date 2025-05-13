f52db1ed4a5538b07a2df628ca91a764554f9cd0
Thimmie-oi <oluwatimilehin@amala.cloud>
2025-05-13 18:43:45 +0100

    New API Added

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 key_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-cred = credentials.Certificate("account_key.json")
+cred = credentials.Certificate(key_path)
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
